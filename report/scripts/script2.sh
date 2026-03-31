dc6806bb326e08ee44e9f01d5efc83528f6c7ac3
RhythmMathur <rhythmmathur2428@gmail.com>
2026-03-31 22:49:00 +0530

    Update script2.sh
    Script 2: FOSS Package Inspector

--- report/scripts/script2.sh
+++ report/scripts/script2.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
+# Script 2: FOSS Package Inspector
 
+PACKAGE="git"
+
+# Check if package is installed
+if dpkg -l | grep -q $PACKAGE; then
+    echo "$PACKAGE is installed."
+    dpkg -l | grep $PACKAGE | awk '{print "Package:", $2, "| Version:", $3}'
+else
+    echo "$PACKAGE is NOT installed."
+fi
+
+# Case statement for description
+case $PACKAGE in
+    git) echo "Git: a distributed version control system built for collaboration";;
+    vlc) echo "VLC: a free media player that plays almost any format";;
+    firefox) echo "Firefox: an open-source web browser focused on privacy";;
+    apache2) echo "Apache: a powerful open-source web server";;
+    *) echo "Unknown package";;
+esac
